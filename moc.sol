c7841684cba262bc6e42b13c09e1bf1ecbf3bbcc
Frajola Puc <frajola.puc@gmail.com>
2020-11-04 15:27:46 -0300

    NOK: Not compiling...
    moc.sol: Not compiling
    test.txt: Just a test.

--- moc.sol
+++ moc.sol
@@ -1,36 +1,100 @@
 pragma solidity ^0.7.0;
+// SPDX-License-Identifier: MIT
+
+
 // Fonte:
 // https://kauri.io/remix-ide-your-first-smart-contract/124b7db1d0cf4f47b414f8b13c9d66e2/a
-contract MasterOwnershipControl {
-    Asset[] public vAsset;
-    
-    struct Asset {
-        address iIdCreator;
-        string sName;
-        string sCPF;
-        string sShortDescription;
-        string sLongDescription;
+
+//0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+
+/*
+contract HelloWorld {
+    function get() public pure returns (string memory){
+        return 'Hello Contracts';
     }
+}
+*/
+/*
+contract HelloWorld {
+
+    string saySomething;
+
+    constructor() {
+        saySomething = "Hello World!";
+    }
+
+    function speak() public view returns(string memory) {
+        return saySomething;
+    }
+
+    function saySomethingElse(string memory newSaying) public  returns(bool success) {
+        saySomething = newSaying;
+        return true;
+    }
+
+}
+*/
+
+contract MasterOwnershipControl {
+    address oOwner;
+    mapping(address => Asset) listAAsset;
     
-    constructor() public {}
+    constructor() { oOwner = msg.sender; }
     
     function newAsset(
         string memory _sName,
         string memory _sCPF,
         string memory _sShortDescription,
         string memory _sLongDescription
-    //) public payable returns(uint256) { 
-    ) public returns(uint256) { 
-        Asset memory oAsset = new Asset(
+    ) public  { 
+        Asset oAsset = new Asset(
             msg.sender,
             _sName,
             _sCPF,
             _sShortDescription,
             _sLongDescription        
             );
-        vAsset.push(oAsset);
-        //return vAsset.length;
-        return (address(oAsset));
+        listAAsset[address(oAsset)] = oAsset;
+    }
+    
+    function updateAsset(
+        address memory _oOwner,
+        string memory _sName,
+        string memory _sCPF,
+        string memory _sShortDescription,
+        string memory _sLongDescription
+    ) public { 
+        oOwner_ = getAssetByAddress(_oOwner);
+        
+        oOwner.updateAsset(
+            _sName,
+            _sCPF,
+            _sShortDescription,
+            _sLongDescription        
+        );
+    }
+
+    function getAssetByAddress(
+        string memory _sCPF
+    ) public view returns(string memory) {
+        for (uint8 i = 0; i < vAsset.length; i++) {
+            if (vAsset[i].sCPF == _sCPF) {
+                return (string(abi.encodePacked(
+                    vAsset[i].sName,
+                    vAsset[i].sCPF,
+                    vAsset[i].sShortDescription,
+                    vAsset[i].sLongDescription
+                )));
+            }
+        }
+        return ("");
+    }
+
+    function getAsset(
+    ) public view returns(string memory) {
+        return (string(abi.encodePacked(
+            iOwner,";",sName,";",sCPF,";",sShortDescription,";",sLongDescription
+            )));
     }
     
     function updateAsset(
@@ -67,3 +131,44 @@ contract MasterOwnershipControl {
         return ("");
     }
 }
+
+contract Asset {
+    address iOwner;
+    string sName;
+    string sCPF;
+    string sShortDescription;
+    string sLongDescription;
+
+    constructor (
+        string memory _sName,
+        string memory _sCPF,
+        string memory _sShortDescription,
+        string memory _sLongDescription
+    )  { 
+        iOwner = msg.sender;
+        sName = _sName;
+        sCPF = _sCPF;
+        sShortDescription = _sShortDescription;
+        sLongDescription = _sLongDescription;
+    }
+    
+    function updateAsset(
+        string memory _sName,
+        string memory _sCPF,
+        string memory _sShortDescription,
+        string memory _sLongDescription
+    ) public { 
+        sName = _sName;
+        sCPF = _sCPF;
+        sShortDescription = _sShortDescription;
+        sLongDescription = _sLongDescription;
+    }
+    
+    function getAsset(
+    ) public view returns(string memory) {
+        return (string(abi.encodePacked(
+            iOwner,";",sName,";",sCPF,";",sShortDescription,";",sLongDescription
+            )));
+    }
+}
+
